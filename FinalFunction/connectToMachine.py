a0a77e92bec8133ce0e68df188834c899817a57e
Brian Rivera <rbrina4@gmail.com>
2023-11-15 01:15:18 -0500

    working

--- FinalFunction/connectToMachine.py
+++ FinalFunction/connectToMachine.py
@@ -7,6 +7,7 @@ import uuid
 import googleapiclient.discovery
 import requests
 
+# URL y encabezados para obtener la dirección de correo electrónico de la cuenta de servicio desde el metadato de la instancia.
 SERVICE_ACCOUNT_METADATA_URL = (
     "http://metadata.google.internal/computeMetadata/v1/instance/"
     "service-accounts/default/email"
@@ -15,55 +16,76 @@ HEADERS = {"Metadata-Flavor": "Google"}
 
 def main(cmds, project, instance=None, zone=None,
          oslogin=None, account=None, hostname=None, username=None,quantity=None):
-    """Run a command on a remote system."""
+    """
+    Ejecuta un comando en un sistema remoto.
 
-    # Create the OS Login API object.
+    Args:
+        cmds: Lista de comandos a ejecutar en el sistema remoto.
+        project: ID del proyecto de GCP.
+        instance: Nombre de la instancia de GCP.
+        zone: Zona donde se encuentra la instancia.
+        oslogin: Cliente del API de OS Login.
+        account: Cuenta de servicio de GCP para autenticación.
+        hostname: Nombre de host del sistema remoto.
+        username: Nombre de usuario POSIX en el sistema remoto.
+        quantity: Parámetro no utilizado en la implementación actual.
+    """
+    
+    # Crear el objeto API de OS Login si no se proporciona.
     oslogin = oslogin or googleapiclient.discovery.build('oslogin', 'v1')
 
-    # Identify the service account ID if it is not already provided.
+    # Identificar el ID de la cuenta de servicio si no se proporciona.
     account = account or requests.get(
         SERVICE_ACCOUNT_METADATA_URL, headers=HEADERS).text
     if not account.startswith('users/'):
         account = 'users/' + account
 
-    # Create a new SSH key pair and associate it with the service account.
+    # Crear un nuevo par de claves SSH y asociarlo con la cuenta de servicio.
     private_key_file = create_ssh_key(oslogin, account)
 
-    # Using the OS Login API, get the POSIX user name from the login profile
-    # for the service account.
+    # Utilizando la API de OS Login, obtener el nombre de usuario POSIX del perfil de inicio de sesión.
     profile = oslogin.users().getLoginProfile(name=account).execute()
-    
     username = username or profile.get('posixAccounts')[0].get('username')
-    print(username)
-    # Create the hostname of the target instance using the instance name,
-    # the zone where the instance is located, and the project that owns the
-    # instance.
+    
+    # Crear el nombre de host de la instancia objetivo usando el nombre de la instancia, la zona y el proyecto.
     hostname = hostname or '{instance}.{zone}.c.{project}.internal'.format(
         instance=instance, zone=zone, project=project)
 
-    # Run a command on the remote instance over SSH.
-    
+    # Ejecutar un comando en la instancia remota a través de SSH.
     result = run_ssh(cmds, private_key_file, username, hostname)
 
-    # Print the command line output from the remote instance.
-    # Use .rstrip() rather than end='' for Python 2 compatability.
+    # Imprimir la salida de la línea de comandos de la instancia remota.
     for line in result:
-        print(line.decode('utf-8').rstrip('\n\r'))
-
-    # Shred the private key and delete the pair.
+        print(line.decode('utf-8').strip())
+    
+    # Destruir la clave privada y eliminar el par de claves.
     execute(['shred', private_key_file])
     execute(['rm', private_key_file])
     execute(['rm', private_key_file + '.pub'])
 
 def create_ssh_key(oslogin, account, private_key_file=None, expire_time=300):
-    """Generate an SSH key pair and apply it to the specified account."""
+    """
+    Genera un par de claves SSH y las aplica a la cuenta especificada.
+
+    Args:
+        oslogin: Objeto cliente del API de OS Login.
+        account: Cuenta de usuario en la que se aplicará la clave SSH.
+        private_key_file: Ruta al archivo de clave privada SSH. Si no se provee, se generará una.
+        expire_time: Tiempo en segundos después del cual la clave expirará.
+
+    Returns:
+        La ruta al archivo de clave privada SSH generado.
+
+    Esta función crea un par de claves SSH, almacena la clave pública en el perfil de OS Login del usuario
+    y devuelve la ruta a la clave privada para su uso en conexiones SSH.
+    """
     private_key_file = private_key_file or '/tmp/key-' + str(uuid.uuid4())
     execute(['ssh-keygen', '-t', 'rsa', '-N', '', '-f', private_key_file])
 
     with open(private_key_file + '.pub', 'r') as original:
         public_key = original.read().strip()
 
-    # Expiration time is in microseconds.
+    # El tiempo de expiración se calcula en microsegundos.
     expiration = int((time.time() + expire_time) * 1000000)
 
     body = {
@@ -74,7 +96,22 @@ def create_ssh_key(oslogin, account, private_key_file=None, expire_time=300):
     return private_key_file
 
 def execute(cmd, cwd=None, capture_output=False, env=None, raise_errors=True):
-    """Execute an external command (wrapper for Python subprocess)."""
+    """
+    Ejecuta un comando externo, es un envoltorio para subprocess de Python.
+
+    Args:
+        cmd: Comando y argumentos a ejecutar como una lista.
+        cwd: Directorio de trabajo actual para el comando.
+        capture_output: Si es True, captura la salida del comando.
+        env: Diccionario de variables de entorno para el comando.
+        raise_errors: Si es True, lanza una excepción si el comando devuelve un error.
+
+    Returns:
+        Una tupla con el código de retorno y la salida del comando.
+
+    Esta función ejecuta un comando en el sistema y opcionalmente captura y devuelve su salida.
+    Si el comando falla y raise_errors es True, se lanza una excepción.
+    """
     logging.info('Executing command: {cmd}'.format(cmd=str(cmd)))
     stdout = subprocess.PIPE if capture_output else None
     process = subprocess.Popen(cmd, cwd=cwd, env=env, stdout=stdout)
@@ -91,7 +128,21 @@ def execute(cmd, cwd=None, capture_output=False, env=None, raise_errors=True):
     return returncode, output
 
 def run_ssh(cmds, private_key_file, username, hostname):
-    """Run a command on a remote system."""
+    """
+    Ejecuta un comando en un sistema remoto a través de SSH.
+
+    Args:
+        cmds: Lista de comandos a ejecutar en el sistema remoto.
+        private_key_file: Ruta al archivo de clave privada SSH.
+        username: Nombre de usuario para la conexión SSH.
+        hostname: Nombre de host del sistema remoto.
+
+    Returns:
+        La salida del comando ejecutado en el sistema remoto.
+
+    Esta función construye y ejecuta un comando SSH, utilizando la clave privada y las credenciales
+    proporcionadas, para correr los comandos especificados en el sistema remoto.
+    """
     ssh_command = [
         'ssh', '-i', private_key_file, '-o', 'StrictHostKeyChecking=no',
         '{username}@{hostname}'.format(username=username, hostname=hostname),
--- FinalFunction/main.py
+++ FinalFunction/main.py
@@ -9,35 +9,89 @@ import google.auth.transport.requests
 import requests
 import math
 
-
 @functions_framework.cloud_event
 def disk_resize(cloud_event):
+    """
+    Esta función se dispara en respuesta a un evento de Cloud Pub/Sub, específicamente diseñado para 
+    redimensionar el disco de una instancia de Compute Engine cuando se alcanza un cierto umbral.
+
+    Args:
+        cloud_event: El evento de Cloud que activó la función, que contiene los datos del mensaje de Pub/Sub.
+
+    Los pasos realizados por la función son:
+    1. Decodificar los datos de Pub/Sub que están en base64 a un objeto JSON.
+    2. Extraer la información relevante como el ID de la instancia, el ID del proyecto y la zona.
+    3. Obtener los detalles de la instancia y determinar el nombre y tamaño actual del disco.
+    4. Calcular el nuevo tamaño del disco basado en el umbral y el valor observado.
+    5. Enviar una solicitud a la API de Compute Engine para cambiar el tamaño del disco.
+    6. Si la solicitud es exitosa, ejecutar comandos en la máquina para ajustar el sistema de archivos.
+    7. Manejar errores y excepciones a lo largo del proceso.
+
+    La función imprime mensajes a lo largo del proceso para proporcionar un registro del progreso y de cualquier error.
+    """
+    # Decodificación de los datos recibidos en el mensaje de Pub/Sub
     pubsub_data = base64.b64decode(cloud_event.data["message"]["data"])
     try:
+        rootPartition_by_interface = {
+            "SCSI": "/dev/sda",
+            "NVME": "/dev/nvme0n1"
+        }
+        # Carga de los datos decodificados como un objeto JSON
         json_data = json.loads(pubsub_data.decode("utf-8"))
+        # Extracción de detalles de la instancia de la carga de datos JSON
         instance_id = json_data["incident"]["resource"]["labels"]["instance_id"]
         project_id = json_data["incident"]["resource"]["labels"]["project_id"]
         zona = json_data["incident"]["resource"]["labels"]["zone"]
+        # Obtención de detalles de la instancia de Compute Engine
         instance_details = get_instance_details(project_id, instance_id, zona)
+        # Conversión de la instancia de detalles a JSON y posteriormente a objeto para su manipulación
         response = compute_v1.Instance.to_json(instance_details)
         response = json.loads(response)
+        # Inicialización de variables para almacenar el nombre y tamaño del disco
         diskName = ""
         disk_size_gb = 0
+        # Extracción de la IP interna de la primera interfaz de red
         internalIP = response["networkInterfaces"][0]["networkIP"]
+        # Búsqueda del disco que coincide con el nombre de la instancia para obtener su tamaño actual
+        partitionX = json_data["incident"]["metric"]["labels"]["device"]
+        print(f"Partición: {partitionX}")
+        print(f"JSON: {json_data}")
+        print(f"response {response}")
         for disk in response.get("disks", []):
-            if disk["deviceName"] == response["name"]:
+            diska= get_disk_details (project_id, disk["deviceName"], zona)
+            response_disk = compute_v1.Disk.to_json(diska)
+            response_disk = json.loads(response_disk)
+            print(f"Disco: {response_disk}")
+            type_disk = response_disk["labels"].get("type")
+            print(f"Tipo de disco: {type_disk}")
+            if type_disk != None and type_disk == "root":
+                type_partition = disk.get('interface')
+                if type_partition == None:
+                    raise Exception("No se encontró el tipo de interfaz del disco")
+                root = rootPartition_by_interface.get(type_partition)
+                print(f"Root: {root}")
                 disk_size_gb = disk.get("diskSizeGb")
                 diskName = disk["deviceName"]
 
+
+        if diskName == "":
+            print("No se encontró el disco de la instancia")
+            raise Exception("No se encontró el disco de la instancia")
+
+        print(f"Disco de la instancia: {diskName}")
+        # Autenticación y preparación para realizar la solicitud de cambio de tamaño del disco
         credentials, your_project_id = google.auth.default(
             scopes=["https://www.googleapis.com/auth/cloud-platform"]
         )
+        # Conversión del tamaño del disco a entero
         disk_size_gb = int(disk_size_gb)
+        # Extracción de valores umbral y observado de los datos del incidente
         threshold_value = float(
             json_data["incident"]["condition"]["conditionThreshold"]["thresholdValue"]
         )
         observerd_value = float(json_data["incident"]["observed_value"])
-
+        
+        # Refresco de credenciales y preparación de la solicitud HTTP
         auth_req = google.auth.transport.requests.Request()
         credentials.refresh(auth_req)
         url = f"https://compute.googleapis.com/compute/v1/projects/{project_id}/zones/{zona}/disks/{diskName}/resize"
@@ -46,61 +100,90 @@ def disk_resize(cloud_event):
             "Authorization": f"Bearer {bearer_token}",
             "Content-Type": "application/json",
         }
-
-        print(f"threshold_value: {threshold_value}")
-        print(f"observerd_value: {observerd_value}")
-        print(f"disk_size_gb: { disk_size_gb}")
+        
+        # Cálculo del nuevo tamaño de disco requerido
         porcentaje = 10
-        b = observerd_value / 100
-        c = (threshold_value+porcentaje) / 100
+        b = (100-observerd_value) / 100
+        c = (100-threshold_value + porcentaje) / 100
         a = disk_size_gb
         x = (c * a - b * a) / (1 - c)
         final_value = math.ceil(disk_size_gb + x)
-        print(f"final_value: {final_value}")
+        
+        # Creación del cuerpo de la solicitud con el nuevo tamaño del disco
         payload = {"sizeGb": str(final_value)}
         newSize = payload["sizeGb"]
+        # Envío de la solicitud POST a la API de Compute Engine para cambiar el tamaño
         response = requests.post(url, headers=headers, data=json.dumps(payload))
+        # Manejo de la respuesta a la solicitud de cambio de tamaño
         if response.status_code == 200:
             print("Request successful")
+            # Si la solicitud es exitosa, se ejecutan comandos en la instancia para ajustar el sistema de archivos
             cn.main(
                 [
-                    "echo -e 'resizepart\nFix\n1\nYes\n100%\nquit' | sudo parted /dev/sda ---pretend-input-tty",
-                    "sudo partprobe /dev/sda",
-                    "sudo resize2fs /dev/sda1",
-                    f"echo 'Se redimensionó el disco {diskName} de {disk_size_gb} GB a {newSize} GB correctamente, quedando con un porcentaje de {threshold_value+porcentaje}% de espacio disponible'",
+                    f"echo -e 'resizepart\nfix\n1\nYes\n100%\nquit' | sudo parted {root} ---pretend-input-tty",
+                    f"sudo partprobe {root}",
+                    f"sudo resize2fs {partitionX}",
+                    f"echo 'Se redimensionó el disco {diskName} de {disk_size_gb} GB a {newSize} GB correctamente, quedando con un porcentaje de {100-threshold_value + porcentaje}% de espacio disponible'",
                 ],
                 project_id,
                 hostname=internalIP,
             )
         else:
+            # Si la solicitud falla, se imprimen mensajes de error
             print(f"Request failed with status code {response.status_code}")
             print(response.text)
 
     except json.JSONDecodeError as e:
+        # Manejo de errores en la decodificación de JSON
         print(f"Error al decodificar los datos JSON: {e}")
-
 
 def get_instance_details(project_id: str, instance_id: str, zone: str) -> dict:
     """
-    Returns detailed information about a specific instance in a project.
-
+    Función auxiliar para obtener detalles de una instancia específica en un proyecto de GCP.
+    
     Args:
-        project_id: Project ID or project number of the Cloud project you want to use.
-        instance_id: ID of the instance you want to retrieve details for.
-        zone: zone of the instace you want to retrieve details for
-
+        project_id: ID del proyecto o número de proyecto en el que se desea operar.
+        instance_id: ID de la instancia de la cual se quieren obtener detalles.
+        zone: Zona en la que se encuentra la instancia.
+    
     Returns:
-        A dictionary with information about the specified instance.
+        Un diccionario con información detallada sobre la instancia especificada.
+    
+    Esta función hace uso de la API de Compute Engine para recuperar los datos de la instancia.
     """
+    # Cliente de la API de Compute Engine
     instance_client = compute_v1.InstancesClient()
+    # Creación de la solicitud para obtener detalles de la instancia
     request = compute_v1.GetInstanceRequest()
+    
+
     request.project = project_id
     request.zone = zone
     request.instance = instance_id
 
     try:
+        # Envío de la solicitud y retorno de la respuesta
         response = instance_client.get(request=request)
         return response
     except Exception as e:
+        # Manejo de errores al obtener detalles de la instancia
         print(f"Error fetching instance details: {e}")
+        return {}
+
+def get_disk_details(project_id: str, disk_name: str, zone: str) -> dict:
+    # Cliente de la API de Compute Engine para discos
+    disk_client = compute_v1.DisksClient()
+    # Creación de la solicitud para obtener detalles del disco
+    request = compute_v1.GetDiskRequest(
+        project=project_id,
+        zone=zone,
+        disk=disk_name  # Asegúrate de que este sea el nombre correcto del disco
+    )
+    try:
+        # Envío de la solicitud y retorno de la respuesta
+        response = disk_client.get(request=request)
+        return response
+    except Exception as e:
+        # Manejo de errores al obtener detalles del disco
+        print(f"Error fetching disk details: {e}")
         return {}
